9c00410f98692978406668e7afc9052663514209
charanraj-thiyagarajan-cdw-com <charanraj.thiyagarajan+cdwcorp@cdw.com>
2025-09-02 17:48:27 +0530

    title added

--- core/src/main/java/com/adobe/aem/tools/contentgenerator/core/servlets/ADKServlet.java
+++ core/src/main/java/com/adobe/aem/tools/contentgenerator/core/servlets/ADKServlet.java
@@ -34,7 +34,7 @@ public class ADKServlet extends SlingAllMethodsServlet {
         String htmlElements = "";
         try {
             Document doc = Jsoup.connect(req.getParameter("externalURL")).get();
-            Elements elements = doc.select("div");
+            Elements elements = doc.select("div,h1");
             StringBuilder inputBuilder = new StringBuilder();
             for (Element el : elements) {
                 inputBuilder.append(el.outerHtml()).append("\n");
